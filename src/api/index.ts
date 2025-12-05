bd884700231c889d398406ab7f6bbb9d81f4672c
BranCuco <bran.escuela@gmail.com>
2025-12-05 15:37:43 -0600

    Index arreglado

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,6 @@ export { default as getWorkers } from './getWorkers';
 export { default as createWorker } from './createWorker';
 export { default as updateWorker } from './updateWorker';
 export { default as deleteWorker } from './deleteWorker';
-export { default as updateWorker } from './updateWorker';
 export { default as getVehicles } from './getVehicles';
 export { default as createVehicle } from './createVehicle';
 export { default as deleteVehicle } from './deleteVehicle';
